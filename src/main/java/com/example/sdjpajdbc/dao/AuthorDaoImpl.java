4784364b1316b11378dd33ec8bd1be36b97911d9
berezkin88 <berezkin05@gmail.com>
2021-12-15 23:33:01 +0200

    retrieved author from db

--- src/main/java/com/example/sdjpajdbc/dao/AuthorDaoImpl.java
+++ src/main/java/com/example/sdjpajdbc/dao/AuthorDaoImpl.java
@@ -3,10 +3,59 @@ package com.example.sdjpajdbc.dao;
 import com.example.sdjpajdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource dataSource;
+
+    public AuthorDaoImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public Author getById(Long id) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from author where id = " + id);
+
+            if (resultSet.next()) {
+                var author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
         return null;
     }
 }
